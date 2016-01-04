7fcf0a73ad74fe30b8fbf81a37d354e5892924b8
manoj <manoj@rudrainnovatives.com>
2016-01-04 13:46:08 +0530

    Update timezone.php

--- timezone.php
+++ timezone.php
@@ -1,2 +1,10 @@
 var timezone = jstz.determine();
-document.cookie="goalsurf_timezone="+timezone.name();
+document.cookie="local_timezone="+timezone.name();
+function localtime($date)
+	{
+    $timezon=isset($_COOKIE['local_timezone'])? $_COOKIE['local_timezone'] : 'Asia/Gaza';
+		$time=strtotime($date);
+		$dt = new DateTime("now", new DateTimeZone($timezon)); //first argument "must" be a string
+		$dt->setTimestamp($time); //adjust the object to correct timestamp
+		return $dt->format('D-m-Y H:i A');
+	}
